525302bbd801112bf2609aeb842b1263da78302b
maomira <141223548+maomira@users.noreply.github.com>
2023-08-02 20:33:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
-window.addEventListener("scroll", function () {
-    const parallax = document.querySelectorAll(".parallax");
-    let scrollPosition = window.pageYOffset;
+// script.js
 
-    parallax.forEach(p => {
-        let offset = p.offsetTop;
-        let windowHeight = window.innerHeight;
-        let parallaxOffset = (scrollPosition - offset) * 0.5;
-        p.style.backgroundPositionY = parallaxOffset + "px";
+// Function to update parallax effect on scroll
+function updateParallax() {
+    const parallaxElements = document.querySelectorAll('.parallax');
+    const menuHeight = document.getElementById('menu').offsetHeight;
+
+    parallaxElements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        const offset = rect.top + rect.height / 2;
+        const scrollPosition = window.pageYOffset + window.innerHeight / 2;
+        const parallaxOffset = (scrollPosition - offset) * 0.5;
+        element.style.setProperty('--parallax-offset', parallaxOffset + 'px');
     });
+}
 
-    // Show the content boxes when scrolling to their respective pages
-    const boxes = document.querySelectorAll(".box, .reelcrafter-box");
-    boxes.forEach(box => {
-        let boxOffset = box.offsetTop;
-        let boxHeight = box.offsetHeight;
-        let boxScrollPosition = scrollPosition + windowHeight * 0.5;
+// Function to add smooth scroll effect to the menu links
+function smoothScroll(target) {
+    const targetElement = document.querySelector(target);
+    const targetPosition = targetElement.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+    const duration = 1000;
 
-        if (boxScrollPosition >= boxOffset && boxScrollPosition <= boxOffset + boxHeight) {
-            box.style.opacity = 1;
-            box.style.transform = "translateY(0)";
-        } else {
-            box.style.opacity = 0;
-            box.style.transform = "translateY(20px)";
-        }
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+// Add event listener to update parallax effect on scroll
+window.addEventListener("scroll", updateParallax);
+
+// Get menu items and add click event listeners
+const menuItems = document.querySelectorAll('.menu a');
+menuItems.forEach(item => {
+    item.addEventListener('click', function (event) {
+        event.preventDefault();
+        const target = this.getAttribute('href');
+        smoothScroll(target);
     });
-
-    // Mouse effect
-    const mouse = document.querySelector(".mouse");
-    mouse.style.left = event.clientX + "px";
-    mouse.style.top = event.clientY + "px";
 });
+
+// Initial call to update parallax effect on page load
+updateParallax();
