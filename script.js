b6438506c4e2c21d523f19c30d4cd570a3f8ea33
maomira <141223548+maomira@users.noreply.github.com>
2023-08-02 18:47:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const pages = document.querySelectorAll(".page");
-
-    window.addEventListener("scroll", function () {
-        const scrollPosition = window.pageYOffset;
-
-        // Update the --parallax-offset variable to move the pages based on scroll
-        pages.forEach((page) => {
-            page.style.setProperty("--parallax-offset", scrollPosition);
-        });
-    });
+window.addEventListener("scroll", function () {
+    const parallax = document.querySelector(".parallax");
+    let scrollPosition = window.pageYOffset;
+    parallax.style.setProperty('--parallax-offset', scrollPosition + 'px');
 });
